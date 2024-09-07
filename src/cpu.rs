f81636d2ca9d54ba8e0606625add4b4af7b55e88
Mahmoud <msobier@mit.edu>
2024-09-07 17:35:51 -0400

    initial commit for neal's branch

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,27 +1,46 @@
 use crate::memory::Memory;
-
+use ux::*;
 pub struct CPU {
     ram : Memory, // 4kB of RAM
     stack : Vec<u16>, // stack, comprising of 2-byte values
-    pc : u16, // program counter
-    index : u16, // index register "I", used to point to addresses in memory
+    pc : u12, // program counter
+    index : u12, // index register "I", used to point to addresses in memory
     // TODO: decide whether to include timers here, or lift them to main.rs
     vs : [u8; 16] // general-purpose registers, labeled V0-VF
 }
 
-
+pub enum Opcode {
+    ClearScreen, 
+    Jump(u12),
+    SetReg(u8),
+    AddReg(u4, u8),
+    SetI(u12),
+    Draw(u4, u4, u4),
+}
 impl CPU {
-    pub fn new() -> Self;
+    pub fn new() -> Self {
+        let ram: Memory = Memory::new(); 
+        
+        CPU {
+            ram : ram,
+            stack : vec![0; 16],
+            pc : 0x200.into(),
+            index : 0x0.into(),
+            vs : [0; 16]
+        }
+    }
 
-    pub fn fetch(self) -> (u8, u8) {
+    
+    pub fn fetch(&mut self) -> (u8, u8) {
         let byte_1 : u8 = self.ram.read(self.pc);
-        let byte_2 : u8 = self.ram.read(self.pc + 1);
-
-        (byte_1, byte_2) 
+        let byte_2 : u8 = self.ram.read(self.pc + 1.into());
+        
+        self.pc = self.pc + 2.into();
+        (byte_1, byte_2)
     } 
 
-    pub fn decode(instr : (u8, u8)) -> ();
+    pub fn decode(instr : (u4, u4, u4, u4)) -> Opcode;
 
-    pub fn execute(instr : (u8, u8)) -> ();
+    pub fn execute(opcode : Opcode) -> ();
 }
 
--- src/memory.rs
+++ src/memory.rs
@@ -1,5 +1,6 @@
 
 use crate::font::FONT_TABLE;
+use ux::*;
 // CHIP-8 was commonly implemented on systems with 4 kB of memory, which we replicate here with an array of bytes
 
 const SIZE : usize = 4096;
@@ -8,37 +9,23 @@ pub struct Memory {
     mem : [u8; SIZE]
 }
 
-// CHIP-8 memory should be 12-bit addressable, so let's implement a function that checks that an address is safe to use for indexing purposes
-pub fn is_twelve_bit(addr : u16) -> bool {
-        let shifted_addr: u16 = addr >> 8;
-        shifted_addr == 0
-}
-
-
-
 impl Memory {
     pub fn new() -> Self {
         let mut mem : [u8; SIZE] = [0; SIZE];
-
         //Copy the contents of the font table into memory
         mem[0..FONT_TABLE.len()].copy_from_slice(&FONT_TABLE); 
-
         Memory { mem : mem }
     }
 
-    pub fn read(&self, addr : u16) -> u8 {
-        if !is_twelve_bit(addr) {
-            panic!("Tried to read into memory with an address wider than 12 bits")
-        }
-
-        self.mem[usize::from(addr)]
+    pub fn read(&self, addr : u12) -> u8 {
+        let wide : u16 = u12::into(addr);
+        let index : usize = u16::into(wide);
+        self.mem[index]
     }
 
-    pub fn write(&mut self, addr : u16, value : u8) -> () {
-        if !is_twelve_bit(addr) {
-            panic!("Tried to write to memory with an address wider than 12 bits")
-        }
-
-        self.mem[usize::from(addr)] = value;
+    pub fn write(&mut self, addr : u12, value : u8) -> () {
+        let wide : u16 = u12::into(addr);
+        let index : usize = u16::into(wide);
+        self.mem[index] = value;
     }
 }
