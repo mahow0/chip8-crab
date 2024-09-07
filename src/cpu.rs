ed33625a314ad4782103a0a010b973a0628444fd
Mahmoud <msobier@mit.edu>
2024-09-07 19:00:32 -0400

    new branch commit

--- src/cpu.rs
+++ src/cpu.rs
@@ -9,14 +9,47 @@ pub struct CPU {
     vs : [u8; 16] // general-purpose registers, labeled V0-VF
 }
 
+
+struct NibblePair(u4, u4); 
+
+impl From<u8> for NibblePair {
+    fn from(value : u8) -> NibblePair {
+        
+        let first_nibble : u4 = (value >> 4).try_into().unwrap();
+        let second_nibble : u4 = (0b0000_1111 & value).try_into().unwrap();
+
+        NibblePair(first_nibble, second_nibble)
+        
+    }
+}
+
+pub fn nibtrio_2_u12(trio : (u4, u4, u4)) -> u12 {
+
+    let mut twelve : u12 = 0.into();
+    let (nib_1, nib_2, nib_3) = trio;
+
+    twelve = twelve  + nib_1.into();
+    twelve = twelve << 8;
+
+    twelve = twelve + nib_2.into();
+    twelve = twelve << 4;
+
+    twelve = twelve + nib_3.into();
+
+    twelve
+}
+
+
 pub enum Opcode {
     ClearScreen, 
     Jump(u12),
-    SetReg(u8),
+    SetReg(u4, u8),
     AddReg(u4, u8),
     SetI(u12),
-    Draw(u4, u4, u4),
+    Display(u4, u4, u4),
 }
+
+
 impl CPU {
     pub fn new() -> Self {
         let ram: Memory = Memory::new(); 
@@ -36,10 +69,40 @@ impl CPU {
         let byte_2 : u8 = self.ram.read(self.pc + 1.into());
         
         self.pc = self.pc + 2.into();
+
         (byte_1, byte_2)
     } 
 
-    pub fn decode(instr : (u4, u4, u4, u4)) -> Opcode;
+    pub fn decode(instr : (u8, u8)) -> Opcode {
+        match instr {
+            (0x00, 0xE0) => Opcode::ClearScreen,
+            (byte_1 @ 0x10..=0x1F, byte_2) => {
+                let nibs_1 : NibblePair = byte_1.into();
+                let NibblePair(_, nib_1) = nibs_1;
+                let NibblePair(nib_2, nib_3) = byte_2.into();
+                Opcode::Jump(nibtrio_2_u12((nib_1, nib_2, nib_3)))
+            }
+            (byte_1 @ 0x60..=0x6F, byte_2) => {
+                let NibblePair(_, nib_1) = byte_1.into();
+                Opcode::SetReg(nib_1, byte_2)
+            }
+            (byte_1 @ 0x70..=0x7F, byte_2) => {
+                let NibblePair(_, nib_1) = byte_1.into();
+                Opcode::AddReg(nib_1, byte_2)
+            }
+            (byte_1 @ 0xA0..=0xAF, byte_2) => {
+                let NibblePair(_, nib_1) = byte_1.into();
+                let NibblePair(nib_2, nib_3) = byte_2.into();
+                Opcode::SetI(nibtrio_2_u12((nib_1, nib_2, nib_3)))  
+            }
+            (byte_1 @ 0xD0..=0xDF, byte_2) => {
+                let NibblePair(_, nib_1) = byte_1.into();
+                let NibblePair(nib_2, nib_3) = byte_2.into();
+                Opcode::Display(nib_1, nib_2, nib_3)
+            }
+            _ => todo!("more instructions")
+        }
+    }
 
     pub fn execute(opcode : Opcode) -> ();
 }
